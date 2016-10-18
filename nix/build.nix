c12f2b1dc84fd43d5b4630b2284ebd1e7e1bc9ce
Tikhon Jelvis <tikhon.jelvis@target.com>
2016-10-17 18:43:08 -0700

    Wrote more code to build Hyper Haskell with Nix.

--- nix/build.nix
+++ nix/build.nix
@@ -3,13 +3,23 @@
 stdenv.mkDerivation rec {
   name = "Hyper-Haskell-${version}";
 
+  # hyper
   hyperProject = import ./haskell.nix {
     inherit stdenv pkgs;
     name = "hyper";
     dir = ../haskell/hyper;
   };
   hyper = pkgs.haskellPackages.callPackage hyperProject {};
-  
+
+  # hyper-extra
+  extraProject = import ./haskell.nix {
+    inherit stdenv pkgs;
+    name = "hyper-extra";
+    dir = ../haskell/hyper-extra;
+  };
+  extra = pkgs.haskellPackages.callPackage extraProject { inherit hyper; };
+
+  # hyper-haskell-server
   serverProject = import ./haskell.nix {
     inherit stdenv pkgs;
     name = "hyper-haskell-server";
@@ -17,8 +27,23 @@ stdenv.mkDerivation rec {
   };
   server = pkgs.haskellPackages.callPackage serverProject { inherit hyper; };
 
-  # inherit src;
-  # buildInputs = [electron ghc cabal-install];
+  electronCommand = if stdenv.isDarwin
+    then "${electron.out}/Applications/Electron.app/Contents/MacOs/Electron"
+    else "${electron.out}/bin/electron";
 
-  
+  inherit src;
+
+  hyperApp = stdenv.mkDerivation ({
+    name = "hyperApp";
+    inherit src;
+
+    buildCommand = ''
+    cp -r $src/app $out;
+    '';
+  });
+
+  buildInputs = [electron ghc cabal-install];
+  buildCommand = ''
+  echo $electronCommand $hyperApp > $out
+  '';
 }
